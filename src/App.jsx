4a01b7cf93011f1f0923fc7cfca2413041611438
Anish Kumar Sharma <anishsharma2779@gmail.com>
2025-08-07 19:29:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,7 @@ export const ProtectedRoute = ({children}) => {
 const ProtectedRouteForAdmin = ({children})=> {
   const admin = JSON.parse(localStorage.getItem('user'))
   
-  if(admin.user.email === 'anishsharma805110@gmail.com'){
+  if(admin.user.email === 'anishsharma@gmail.com'){
     return children
   }
   else{
