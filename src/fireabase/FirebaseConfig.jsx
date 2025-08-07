9da22b79354ccfb45465618550be5d47a7444425
Anish Kumar Sharma <anishsharma2779@gmail.com>
2025-08-07 19:49:24 +0530

    Update FirebaseConfig.jsx

--- src/fireabase/FirebaseConfig.jsx
+++ src/fireabase/FirebaseConfig.jsx
@@ -6,12 +6,18 @@ import {getAuth} from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDoJvalILWn6esetahpL9BGRT__B5azWvI",
-  authDomain: "myfirstapp-38751.firebaseapp.com",
-  projectId: "myfirstapp-38751",
-  storageBucket: "myfirstapp-38751.appspot.com",
-  messagingSenderId: "273202707457",
-  appId: "1:273202707457:web:5724f212508b5b82f31400"
+  // apiKey: "AIzaSyDoJvalILWn6esetahpL9BGRT__B5azWvI",
+  // authDomain: "myfirstapp-38751.firebaseapp.com",
+  // projectId: "myfirstapp-38751",
+  // storageBucket: "myfirstapp-38751.appspot.com",
+  // messagingSenderId: "273202707457",
+  // appId: "1:273202707457:web:5724f212508b5b82f31400"
+  apiKey: "AIzaSyBETuugespXnY-bwHUK6jd9adh1JOl1TNI",
+  authDomain: "caphub-7280c.firebaseapp.com",
+  projectId: "caphub-7280c",
+  storageBucket: "caphub-7280c.firebasestorage.app",
+  messagingSenderId: "692782326738",
+  appId: "1:692782326738:web:2c2ab825af7705a86cde0b",
 };
 
 // Initialize Firebase
